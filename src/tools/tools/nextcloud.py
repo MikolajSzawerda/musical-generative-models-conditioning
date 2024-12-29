bf806103f9d865d550ad8901e4cba45b317a8a48
Mikołaj Szawerda <96917430+MikolajSzawerda@users.noreply.github.com>
2024-12-29 20:17:59 +0100

    add clip model (#12)

--- src/tools/tools/nextcloud.py
+++ src/tools/tools/nextcloud.py
@@ -5,12 +5,13 @@ import logging
 import argparse
 from rich.console import Console
 
+
 def _validate_env():
-    if not os.getenv('STORAGE_URL'):
+    if not os.getenv("STORAGE_URL"):
         raise Exception("no storage url provided")
-    if not os.getenv('STORAGE_LOGIN'):
+    if not os.getenv("STORAGE_LOGIN"):
         raise Exception("no storage login provided")
-    if not os.getenv('STORAGE_PASSWORD'):
+    if not os.getenv("STORAGE_PASSWORD"):
         raise Exception("no storage password provided")
 
 
@@ -19,54 +20,65 @@ class NextCloud:
         logging.basicConfig(level=logging.INFO)
         load_dotenv(find_dotenv())
         _validate_env()
-        self.client = Client({
-            'webdav_hostname': os.getenv('STORAGE_URL'),
-            'webdav_login': os.getenv('STORAGE_LOGIN'),
-            'webdav_password': os.getenv('STORAGE_PASSWORD')
-        })
+        self.client = Client(
+            {
+                "webdav_hostname": os.getenv("STORAGE_URL"),
+                "webdav_login": os.getenv("STORAGE_LOGIN"),
+                "webdav_password": os.getenv("STORAGE_PASSWORD"),
+            }
+        )
         self.log: Console = logger
+
     def _log(self, *args, **kwargs):
         if self.log:
             self.log.print(*args, **kwargs)
+
     def _create_remote_directory(self, remote_path):
         if self.client.check(remote_path):
             return
         self._log("[yellow]Have to create recursivly directories")
-        directories = remote_path.strip('/').split('/')
-        current_dir = ''
+        directories = remote_path.strip("/").split("/")
+        current_dir = ""
         for directory in directories:
-            current_dir += f'{directory}/'
+            current_dir += f"{directory}/"
             if not self.client.check(current_dir):
                 self.client.mkdir(current_dir)
 
+    def upload_file(self, src: str, dest: str):
+        self.client.upload_file(dest, src)
+        self._log("[green]Upload finished :smiley:")
+
     def upload(self, src: str, dest: str):
-        path = f'thesis/storage/{dest}'
+        path = f"thesis/storage/{dest}"
         self._create_remote_directory(path)
         if not self.client.push(path, src):
             self._log("[yellow]No files to upload")
         self._log("[green]Upload finished :smiley:")
 
     def download(self, src: str, dest: str):
-        path = f'thesis/storage/{src}'
+        path = f"thesis/storage/{src}"
         if not self.client.pull(path, dest):
             self._log("[yellow]No files to download")
         self._log("[green]Download finished :smiley:")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("method")
-    parser.add_argument("src")
-    parser.add_argument("dest", nargs='?')
+    parser.add_argument("--method")
+    parser.add_argument("--src")
+    parser.add_argument("--dest", nargs="?")
     args = parser.parse_args()
     load_dotenv(find_dotenv())
     _validate_env()
     options = {
-        'webdav_hostname': os.getenv('STORAGE_URL'),
-        'webdav_login': os.getenv('STORAGE_LOGIN'),
-        'webdav_password': os.getenv('STORAGE_PASSWORD')
+        "webdav_hostname": os.getenv("STORAGE_URL"),
+        "webdav_login": os.getenv("STORAGE_LOGIN"),
+        "webdav_password": os.getenv("STORAGE_PASSWORD"),
     }
     nc = NextCloud(Console())
-    if args.method == 'push':
+    if args.method == "push":
         nc.upload(args.src, args.dest if args.dest else args.src)
-    if args.method == 'pull':
+    if args.method == "push-file":
+        nc.upload_file(args.src, args.dest if args.dest else os.path.basename(args.src))
+    if args.method == "pull":
         nc.download(args.src, args.dest if args.dest else args.src)
